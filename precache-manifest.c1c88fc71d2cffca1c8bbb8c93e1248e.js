55c1cb3067fc373818c378a72a53e7e53f341b1d
IMRANDIL <aliimranadil2@gmail.com>
2021-06-25 16:41:13 +0530

    Updates

---
+++ precache-manifest.c1c88fc71d2cffca1c8bbb8c93e1248e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ed9cab0f6e3df4cbbadddaeab50470",
+    "url": "/Navbar_React/index.html"
+  },
+  {
+    "revision": "905788bfc53247d8ae2a",
+    "url": "/Navbar_React/static/css/main.beaf029b.chunk.css"
+  },
+  {
+    "revision": "5036664f6a0cc62b8b04",
+    "url": "/Navbar_React/static/js/2.41d20619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Navbar_React/static/js/2.41d20619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905788bfc53247d8ae2a",
+    "url": "/Navbar_React/static/js/main.40df8e1f.chunk.js"
+  },
+  {
+    "revision": "d7a1d8b0faa8ef0eb21e",
+    "url": "/Navbar_React/static/js/runtime-main.ae993935.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/Navbar_React/static/media/logo.2bb7da65.svg"
+  }
+]);
